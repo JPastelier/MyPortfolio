9af85de6ce286c7aa6013e3b03bff7b205c65bc3
JPastelier <shadonkin@gmail.com>
2025-01-14 13:41:14 -0700

    Updated portfolio

--- src/data/data.js
+++ src/data/data.js
@@ -1,13 +1,13 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Doe",
-    bio: "Undergraduate Architectural Student at University of Calgary passionate about building tools to make homes more affordable.",
+    name: "John Paul",
+    bio: "im a baka.",
     contact: {
-        email: "john.doe@gmail.com",
+        email: "shadonkin@gmail.com",
         phone: "+12345678910",
         linkedin: "https://www.linkedin.com/",
-        github: "https://www.github.com/",
+        github: "https://github.com/JPastelier?tab=repositories",
     }
 }
 
