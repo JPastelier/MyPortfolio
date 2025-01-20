598868d399710d5c0e042b7978527f9c4729ef9f
JPastelier <shadonkin@gmail.com>
2025-01-19 20:30:31 -0700

    'message'

--- src/data/data.js
+++ src/data/data.js
@@ -1,13 +1,12 @@
 // CUSTOMIZE THIS FILE TO ADD YOUR OWN DATA
 
 const data = {
-    name: "John Paul",
-    bio: "im a baka.",
+    name: "John Paul Echavez",
+    bio: "Undergraduate Architectural Student at University of Calgary.",
     contact: {
-        email: "shadonkin@gmail.com",
-        phone: "+12345678910",
-        linkedin: "https://www.linkedin.com/",
-        github: "https://github.com/JPastelier?tab=repositories",
+        email: "johnpaul.echavez@ucalgary.ca",
+        phone: "+14034028339",
+        github: "https://github.com/JPastelier/MyPortfolio",
     }
 }
 
